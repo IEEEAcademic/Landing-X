0733f9dbc9a27b5e81d9ff8bac09b87b9725d4a1
Alexander <knuxus@gmail.com>
2013-05-29 09:39:53 +0100

    initial commit

---
+++ mailer.php
@@ -0,0 +1,16 @@
+<?php
+$email = $_POST['email'];
+ 
+$to = 'knuxus@gmail.com';
+$subject = 'academic x';
+$message = 'FROM: '.$email;
+$headers = 'From: academic@academic.com' . "\r\n";
+ 
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {  
+mail($to, $subject, $message, $headers);  
+echo "Your email was sent!";  
+}else{
+echo "Invalid Email, please provide an correct email.";
+}
+
+?>
